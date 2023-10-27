d2fe56870f608461396a6f6ae2df688801eed771
Sergio Villén Galera <sergio.villen@gmail.com>
2023-10-27 12:48:18 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,42 @@
+function skillsMember() {
+  // Path: member.js
+  skillsMember = function () {
+    var member = Member.findOne({userId: Meteor.userId()});
+    return member.skills;
+  };
+  // Path: member.js
+  skillsMember = function () {
+    var member = Member.findOne({userId: Meteor.userId()});
+    return member.skills;
+  };
+  // Path: member.js
+  skillsMember = function () {
+    var member = Member.findOne({userId: Meteor.userId()});
+    return member.skills;
+  };
+  // Path: member.js
+  skillsMember = function () {
+    var member = Member.findOne({userId: Meteor.userId()});
+    return member.skills;
+  };
+  // Path: member.js
+  skillsMember = function () {
+    var member = Member.findOne({userId: Meteor.userId()});
+    return member.skills;
+  };
+  // Path: member.js
+  skillsMember = function () {
+    var member = Member.findOne({userId: Meteor.userId()});
+    return member.skills;
+  };
+  // Path: member.js
+  skillsMember = function () {
+    var member = Member.findOne({userId: Meteor.userId()});
+    return member.skills;
+  };
+  // Path: member.js
+  skillsMember = function () {
+    var member = Member.findOne({userId: Meteor.userId()});
+    return member.skills;
+  };
+}
